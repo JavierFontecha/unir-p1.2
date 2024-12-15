978f5ddf292696c38205bf34584b0cef1e73b294
Javier Fontecha Guadaño <jfontecha@gmv.com>
2024-12-16 00:56:34 +0100

    added rest and unit tests for multiply and division

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,8 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
+
 
 import pytest
 
@@ -34,6 +36,45 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "8", "ERROR SQRT"
         )
+    
+    def test_api_multiply(self):
+        url = f"{BASE_URL_MOCK}/calc/multiply/2/3"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "6", "ERROR MULTIPLY"
+        )
+    
+    def test_api_divide(self):
+        url = f"{BASE_URL_MOCK}/calc/divide/2/2"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "1", "ERROR DIVIDE"
+        )
+        
+    def test_api_divide_cero(self):
+        url = f"{BASE_URL_MOCK}/calc/divide/2/0"
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.fail(f"Se esperaba un error HTTP pero la respuesta fue: {response.status}")
+        except HTTPError as e:
+            # Verificamos http 406
+            self.assertEqual(
+                e.code,
+                http.client.NOT_ACCEPTABLE,
+                f"Se esperaba un código 406 pero se recibió {e.code} al llamar a {url}"
+            )
+            self.assertEqual(
+                e.read().decode(),
+                "Division by zero is not allowed",
+                "El mensaje de error no coincide con lo esperado"
+            )
+
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
